2a6602b7caccf041650aaaff31bc7e7a866644a2
Dustin Hunter <102769956+LacunaStrategies@users.noreply.github.com>
2022-04-01 16:09:26 -0700

    Switched Demo to Whitelist
    Testing whitelist functionality
    - Compiled and deployed new contract
    - Updated contract address
    - Updated abi

--- src/components/MintButton.js
+++ src/components/MintButton.js
@@ -1,7 +1,8 @@
 import { ethers } from "ethers";
-import demoNft from '../utils/DemoNFT.json';
+import whitelistNFT from '../utils/WhitelistNFT.json';
 
-const CONTRACT_ADDRESS = "0xa2AAa19041C4f56E6fa098273276fB4BF5B5BDb4";
+//const CONTRACT_ADDRESS = "0xa2AAa19041C4f56E6fa098273276fB4BF5B5BDb4";
+const CONTRACT_ADDRESS = "0x3345EBf437b1993ef446C5052c65F8AE82B4a35c";
 
 const MintButton = () => {
 
@@ -9,13 +10,27 @@ const MintButton = () => {
     try {
       const { ethereum } = window;
 
+      const coupon = {
+        "r":"0x80ff20f07ab1da913d57966eebb57b2f3db0207ed181531f6fe776a1dd8c8de1",
+        "s":"0x7a4f19d0859a2022e63005f1f0f404a50ea06e00399d11448849e8b99ffb6092",
+        "v":27
+      }
+      const allotted = 2;
+      const qty = 1;
+      const priceInEth = 0.0001;
+      
+
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, demoNft.abi, signer);
-
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, whitelistNFT.abi, signer);
+        
         console.log("Going to pop wallet now to pay gas...")
-        let nftTxn = await connectedContract.mint();
+
+        const mintPriceBn = ethers.utils.parseEther(priceInEth.toString());
+        console.log('Multiplier:' + mintPriceBn.mul(qty));
+        
+        let nftTxn = await connectedContract.mintPresale(qty,allotted,coupon,mintPriceBn.mul(qty));
 
         console.log("Mining...please wait.")
         await nftTxn.wait();
